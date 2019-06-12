dec229107a9d5772c2c6e66e94dbf1243670de8a
Sam Kearney <samuelmkearney@gmail.com>
2019-06-11 22:03:36 -0500

    *VERY* minimal working example

--- examples/send/main.rs
+++ examples/send/main.rs
@@ -1,10 +1,13 @@
-extern crate sacn;
 extern crate get_if_addrs;
+extern crate sacn;
 
+use get_if_addrs::get_if_addrs;
 use sacn::DmxSource;
 use std::io::{self, Write};
-use get_if_addrs::get_if_addrs;
+use std::sync::mpsc;
+use std::{error, fmt, num, thread, time};
 
+#[derive(Debug)]
 enum GetInterfaceError {
     IoError(io::Error),
     ParseError(num::ParseIntError),
@@ -22,61 +25,100 @@ impl From<num::ParseIntError> for GetInterfaceError {
     }
 }
 
-/// Enumerate the network interfaces on the system and prompt the user
-/// for their selection.
+impl fmt::Display for GetInterfaceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            GetInterfaceError::IoError(ref e) => e.fmt(f),
+            GetInterfaceError::ParseError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for GetInterfaceError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            GetInterfaceError::IoError(ref e) => Some(e),
+            GetInterfaceError::ParseError(ref e) => Some(e),
+        }
+    }
+}
+
+/// Enumerate the network interfaces on the system and prompt the user for
+/// their selection.
 fn get_interface_to_use() -> Result<String, GetInterfaceError> {
-  // Find the interfaces
-  let if_addrs = get_if_addrs()?;
-  if if_addrs.is_empty() {
-    return Err(io::Error::new(io::ErrorKind::Other, "No network interfaces found"));
-  }
+    // Find the interfaces
+    let if_addrs = get_if_addrs()?;
+    if if_addrs.is_empty() {
+        return Err(GetInterfaceError::from(io::Error::new(
+            io::ErrorKind::Other,
+            "No network interfaces found",
+        )));
+    }
 
-  // Print and prompt for selection
-  println!("Found these network interfaces:");
-  for (i, if_addr) in if_addrs.iter().enumerate() {
-    println!("{}: {}", i, if_addr.ip().to_string());
-  }
-  print!("Select an interface: ");
-  io::stdout().flush()?;
-  let mut line = String::new();
-  io::stdin().read_line(&mut line)?;
+    // Print and prompt for selection
+    println!("Found these network interfaces:");
+    for (i, if_addr) in if_addrs.iter().enumerate() {
+        println!("{}: {}", i, if_addr.ip().to_string());
+    }
+    print!("Select an interface: ");
+    io::stdout().flush()?;
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
 
-  // Parse the result
-  match line.parse::<usize>() {
-    Ok(index) => Ok(if_addrs[index].ip().to_string()),
-    Err(e) => Err(e)
-  }
+    // Parse the result
+    match line.trim().parse::<usize>() {
+        Ok(index) => Ok(if_addrs[index].ip().to_string()),
+        Err(e) => Err(GetInterfaceError::from(e)),
+    }
 }
 
 fn main() {
+    let int_ip = match get_interface_to_use() {
+        Ok(ip) => ip,
+        Err(e) => {
+            println!("Error getting network interface: {}", e);
+            std::process::exit(1);
+        }
+    };
 
-  let int_ip = match get_interface_to_use() {
-    Ok(ip) => ip,
-    Err(e) => {
-      println!("Error getting network interface: {}", e);
-      std::process::exit(1);
+    let dmx_source = DmxSource::with_ip("Controller", &int_ip).unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    let dmx_thread = thread::spawn(move || {
+        loop {
+            match dmx_source.send(1, &[0, 1, 2]) {
+                Ok(_) => {}
+                Err(_) => {
+                    println!("Send error... send thread terminating.");
+                    break;
+                }
+            }
+
+            thread::sleep(time::Duration::from_millis(22));
+
+            match rx.try_recv() {
+                Ok(_) | Err(mpsc::TryRecvError::Disconnected) => {
+                    println!("Send thread terminating.");
+                    break;
+                }
+                Err(mpsc::TryRecvError::Empty) => {}
+            }
+        }
+
+        // terminate the stream for a specific universe
+        match dmx_source.terminate_stream(1) {
+            _ => (),
+        }
+    });
+
+    println!("Press any key to exit...");
+    let mut line = String::new();
+    match io::stdin().read_line(&mut line) {
+        _ => (),
     }
-  };
 
-  let dmx_source = DmxSource::new("Controller").unwrap();
+    let _ = tx.send(());
 
-  match dmx_source.send(1, &[0, 1, 2]) {
-    Ok(_) => {
-      println!("DMX transmission started.")
-    }
-    Err(_) => {
-      println!("Error sending DMX to universe 1!");
-    }
-  }
-
-  println!("Press any key to exit...");
-  let mut line = String::new();
-  match io::stdin().read_line(&mut line) {
-    _ => ()
-  }
-
-  // terminate the stream for a specific universe
-  match dmx_source.terminate_stream(1) {
-    _ => ()
-  }
+    dmx_thread.join().unwrap();
 }
